a38548bde93093121c1c2e610d2660e1a056a929
iainjmitchell <iainjmitchell@gmail.com>
2014-12-23 12:13:36 +0000

    1 is happy

--- happy-tests.rb
+++ happy-tests.rb
@@ -5,4 +5,13 @@ class TestHappyNumbers < Test::Unit::TestCase
 		assert_equal true, true	
 	end
 
+	def test_1_is_happy
+		assert Mathematician.new.is_happy?(1)
+	end
 end
+
+class Mathematician
+	def is_happy?(number)
+		true
+	end
+end	
