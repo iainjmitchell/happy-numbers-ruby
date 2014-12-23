a05cdf9c1c692d63af21de148dd6080375cfcc6f
iainjmitchell <iainjmitchell@gmail.com>
2014-12-23 12:24:23 +0000

    refactoring

--- happy-tests.rb
+++ happy-tests.rb
@@ -1,16 +1,17 @@
 require 'test/unit'
 
 class TestHappyNumbers < Test::Unit::TestCase
-	def test_1_is_happy
-		assert_equal Mathematician.new.is_happy?(1), true
+	
+	def test_happy_numbers
+		[1, 10].each do |happy_number|
+			assert_equal Mathematician.new.is_happy?(happy_number), true
+		end
 	end
 
-	def test_10_is_happy
-		assert_equal Mathematician.new.is_happy?(10), true
-	end
-
-	def test_2_is_sad
-		assert_equal Mathematician.new.is_happy?(2), false
+	def test_sad_numbers
+		[2].each do |sad_number|
+			assert_equal Mathematician.new.is_happy?(sad_number), false
+		end
 	end
 end
 
