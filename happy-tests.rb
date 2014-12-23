53ccde1b9dad08f7aaf61435c2d2700a8fd96cb3
iainjmitchell <iainjmitchell@gmail.com>
2014-12-23 12:17:15 +0000

    2 is sad

--- happy-tests.rb
+++ happy-tests.rb
@@ -1,17 +1,18 @@
 require 'test/unit'
 
 class TestHappyNumbers < Test::Unit::TestCase
-	def test_something
-		assert_equal true, true	
+	def test_1_is_happy
+		assert_equal Mathematician.new.is_happy?(1), true
 	end
 
-	def test_1_is_happy
-		assert Mathematician.new.is_happy?(1)
+	def test_2_is_sad
+		assert_equal Mathematician.new.is_happy?(2), false
 	end
 end
 
 class Mathematician
 	def is_happy?(number)
-		true
+		return true if number == 1
+		return false	
 	end
 end	
